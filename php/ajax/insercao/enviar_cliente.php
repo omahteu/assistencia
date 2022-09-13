c9654f1b14231a4dbc5f92be03f06bd787a9d761
Matheus <matheus.mmn@hotmail.com>
2022-09-13 09:34:02 -0300

    php atualizado cadastro de clientes

--- php/ajax/insercao/enviar_cliente.php
+++ php/ajax/insercao/enviar_cliente.php
@@ -3,14 +3,16 @@
     $nome = $_POST["nome"];
     $telefone = $_POST["telefone"];
     $endereco = $_POST["endereco"];
+    $numero = $_POST["numero"];
+    $bairro = $_POST["bairro"];
 
     $conn = new mysqli('us125-cp.valueserver.com.br','fortal_adm_garantias','{xwl&EGy]o(#','fortal_garantias', 3306);
         if($conn->connect_error){
             echo "$conn->connect_error";
             die("Connection Failed : ". $conn->connect_error);
         } else {
-            $stmt = $conn->prepare("insert into clientes(cpfcnpj, nome, telefone, endereco) values(?, ?, ?, ?)");
-            $stmt->bind_param("ssss", $cpfcnpj, $nome, $telefone, $endereco);
+            $stmt = $conn->prepare("insert into clientes(cpfcnpj, nome, telefone, rua, numero, bairro) values(?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("ssssss", $cpfcnpj, $nome, $telefone, $endereco, $numero, $bairro);
             $execval = $stmt->execute();
             echo $execval;
             echo "Registration successfully...";
