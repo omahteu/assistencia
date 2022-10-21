b55f29c2860d4990016eec44817fc8f91aed50f0
matheus <matheus.mmn@hotmail.com>
2022-10-21 14:49:49 -0300

    php enviar clientes

--- php/ajax/insercao/enviar_cliente.php
+++ php/ajax/insercao/enviar_cliente.php
@@ -5,18 +5,18 @@
     $endereco = $_POST["endereco"];
 
     $conn = new mysqli('us125-cp.valueserver.com.br','fortal_adm_garantias','{xwl&EGy]o(#','fortal_garantias', 3306);
-        if($conn->connect_error){
-            echo "$conn->connect_error";
-            die("Connection Failed : ". $conn->connect_error);
-        } else {
-            $stmt = $conn->prepare("insert into clientes(cpfcnpj, nome, telefone, endereco) values(?, ?, ?, ?)");
-            $stmt->bind_param("ssss", $cpfcnpj, $nome, $telefone, $endereco);
-            $execval = $stmt->execute();
-            echo $execval;
-            echo "Registration successfully...";
-            sleep(2);
-            header("Location: https://fortalconnect.com.br/cadastro.html");
-            $stmt->close();
-            $conn->close();
-        }
+    if($conn->connect_error){
+        echo "$conn->connect_error";
+        die("Connection Failed : ". $conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("insert into clientes(cpfcnpj, nome, telefone, endereco) values(?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $cpfcnpj, $nome, $telefone, $endereco);
+        $execval = $stmt->execute();
+        echo $execval;
+        echo "Registration successfully...";
+        sleep(2);
+        header("Location: https://fortalconnect.com.br/cadastro.html");
+        $stmt->close();
+        $conn->close();
+    }
 ?> 
